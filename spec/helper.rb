ef3133451c63db0d215b628acb50cfd8c1bbbbbe
John Nunemaker <nunemaker@gmail.com>
2012-11-09 12:01:24 -0500

    Added logging to executor.

--- spec/helper.rb
+++ spec/helper.rb
@@ -3,19 +3,29 @@ $:.unshift(File.expand_path('../', __FILE__))
 
 require 'rubygems'
 require 'bundler'
+require 'logger'
+require 'pathname'
 
 Bundler.require(:default, :test)
 
 require 'adapter/spec/an_adapter'
 require 'adapter-cassanity'
 
+log_path = Pathname(__FILE__).join('..', '..', 'log').expand_path
+log_path.mkpath
+
+logger = Logger.new(log_path.join('test.log'))
+
 COLUMN_FAMILIES = {}
 
 cassandra_setup = lambda { |args|
   host          = ENV.fetch('CASSANDRA_HOST', '127.0.0.1:9160')
   keyspace_name = ENV.fetch('CASSANDRA_KEYSPACE_NAME', 'adapter_cassanity')
-  client        = CassandraCQL::Database.new(host)
-  executor      = Cassanity::Executors::CassandraCql.new(client: client)
+  client        = CassandraCQL::Database.new(host, cql_version: '3.0.0')
+  executor      = Cassanity::Executors::CassandraCql.new({
+    client: client,
+    logger: logger,
+  })
   connection    = Cassanity::Connection.new(executor: executor)
   keyspace      = connection.keyspace(keyspace_name)
   keyspace.recreate
